e7847d3556bb855f671483a6544399986eadd7e7
Lexi Farris <lexifarris@Lexis-Laptop.local>
2024-07-23 16:12:50 -0600

    connected FastAPI to PostgreSQL

---
+++ backend/app/core/tasks.py
@@ -0,0 +1,16 @@
+#this file wraps up start up and shut down events for the app 
+
+from typing import Callable
+from fastapi  import FastAPI 
+
+from app.db.tasks import connect_to_db, close_db_connection
+
+def create_start_app_handler(app: FastAPI) -> Callable:
+  async def start_app() -> None:
+    await connect_to_db(app)
+  return start_app
+
+def create_stop_app_handler(app: FastAPI) -> Callable:
+  async def stop_app() -> None:
+    await close_db_connection(app)
+  return stop_app 
