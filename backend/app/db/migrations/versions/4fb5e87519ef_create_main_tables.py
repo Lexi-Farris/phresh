57df3aeb26dbcf3f144cd1bac3cc9d16c4509a9e
Lexi-Farris <mlexifarris@gmail.com>
2024-10-02 10:51:23 -0500

    migrated db

---
+++ backend/app/db/migrations/versions/4fb5e87519ef_create_main_tables.py
@@ -0,0 +1,26 @@
+"""create_main_tables
+ 
+Revision ID: 4fb5e87519ef
+Revises: 
+Create Date: 2024-10-02 15:47:47.044272
+ 
+"""
+from alembic import op
+import sqlalchemy as sa
+
+ 
+# revision identifiers, used by Alembic
+revision = '4fb5e87519ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+ 
+ 
+def upgrade() -> None:
+    pass
+ 
+ 
+def downgrade() -> None:
+    pass
+ 
+ 
