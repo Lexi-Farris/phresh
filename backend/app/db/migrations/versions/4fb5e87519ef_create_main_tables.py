7112486d4bd6ec0cc730314e7cc08f6022491fe2
Lexi-Farris <mlexifarris@gmail.com>
2024-10-02 15:26:49 -0500

    added create_cleanings_table function)

--- backend/app/db/migrations/versions/4fb5e87519ef_create_main_tables.py
+++ backend/app/db/migrations/versions/4fb5e87519ef_create_main_tables.py
@@ -14,7 +14,16 @@ revision = '4fb5e87519ef'
 down_revision = None
 branch_labels = None
 depends_on = None
- 
+
+def create_cleanings_table() -> None: 
+    op.create_table(
+        "cleanings"
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.Text, nullable=False, index=True),
+        sa.Column("description", sa.Text, nullable=True),
+        sa.Column("cleaning_type", sa.Text, nullable=False, server_default="spot_cleaning"),
+        sa.Column("price", sa.Numeric(10,2), nullable=False, )
+    )
  
 def upgrade() -> None:
     pass
